806b8a3f7e7ba27307f79bfb994d5994d5cefc59
unfor <unforu0@gmail.com>
2023-02-15 04:35:46 +0300

    added migrations

---
+++ database/migrations/2023_02_15_012547_create_people_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20);
+            $table->string('surname', 25);
+            $table->string('patronym', 25);
+            $table->text('img')->nullable();
+            $table->string('email')->unique();
+            $table->string('phone', 20)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+};
