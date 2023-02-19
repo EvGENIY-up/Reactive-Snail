7bda687dc02e63456959b5482dc8298ce04e3eb1
unfor <unforu0@gmail.com>
2023-02-19 11:46:20 +0300

    added app vue component

---
+++ resources/js/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+   <Header/>
+   <router-view></router-view>
+   <Footer/>
+</template>
+
+<script>
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+export default {
+    name: "App",
+    components: {
+            Header,
+            Footer,
+        },
+    mounted() {
+        console.log('Component mounted.')
+    },
+    }
+</script>
