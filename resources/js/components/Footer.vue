fd4cdb34c2d3e2416e009449ea46e442c3876ed0
unfor <unforu0@gmail.com>
2023-02-19 11:49:28 +0300

    added footer vue component

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="info d-flex justify-content-between m-4 mt-4 mb-5 border-top">
+        <h5 class="info__title">Made by Evgeniy for Stavtrack</h5>
+        <a href="" class="info__mail">unforu0@gmail.com</a>
+    </div>
+</template>
+
+<script>
+export default {
+        name: "Footer",
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
