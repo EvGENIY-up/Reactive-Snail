d9bfb2024cb03d2b41ed65712046c1b5109a62be
unfor <unforu0@gmail.com>
2023-02-21 12:03:08 +0300

    added car component (item)

--- resources/js/components/Car/IndexCar.vue
+++ resources/js/components/Car/IndexCar.vue
@@ -29,7 +29,7 @@ export default {
     methods: {        
         getAllCars() {
             axios.get('/api/cars').then((response) => {
-                this.drivers = response.data.data
+                this.cars = response.data.data
                 console.log(response.data.data);
             }).catch((error) => {
                 console.log(error)
